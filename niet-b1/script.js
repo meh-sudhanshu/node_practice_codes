79788d7a825a159bd9944d558b1a5f4c1089357d
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-10-23 11:42:59 +0530

    More codes

--- niet-b1/script.js
+++ niet-b1/script.js
@@ -2,18 +2,46 @@
 
 
 
-const url = "https://wttr.in/London?format=j1"
-const headers = {
-    'Accept': 'text/html',
-    'Content-Type': 'text/html',
-}
+// const url = "https://wttr.in/London?format=j1"
+// const headers = {
+//     'Accept': 'text/html',
+//     'Content-Type': 'text/html',
+// }
 
 
-for(var i = 0;i < 10; i++){
-    const response = fetch(url)
-response.then(res=> res.json()).then((data)=>{
-    console.log(data)
-}).catch((err)=>{
-    console.log(err)
-})
+
+// const getDataFromApi = async ()=>{
+//     const response = await fetch(url)
+//     const data = await response.json()
+//     console.log(data)
+// }
+
+// getDataFromApi()
+
+// // const response = fetch(url)
+// // response.then(res=> res.json()).then((data)=>{
+// //     console.log(data)
+// // }).catch((err)=>{
+// //     console.log(err)
+// // })
+
+
+
+//  unspalsh started ---------------------------
+
+
+
+const baseUrl = "https://api.unsplash.com/"
+const accessKey = "U0yUIBF6JLuW1KIyz2Z9Fqd3ZZWLHzY3QjfCVsRAEBo"
+const accessKeyString = "?client_id="+accessKey
+
+const api = baseUrl+"/photos/"+accessKeyString
+
+
+const getARandomPhoto = async ()=>{
+    const response = await fetch(api)
+    const data = await response.json()
+    console.log(data[0].links.self)
 }
+
+getARandomPhoto()
