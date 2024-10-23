f6ddf9a417a5d214895e60f2f1d9fb2b36bef86c
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-10-23 15:06:38 +0530

    more codes

--- niet-b2/script.js
+++ niet-b2/script.js
@@ -4,13 +4,22 @@
 
 const api = "http://universities.hipolabs.com/search?country=United+States"
 
-const response =  fetch(api)
 
-response.then(res=>res.json()).then(data=>{
-    console.log(data[0].country)
-}).catch(err=>{
-    console.log(err)
-})
+const getApiData = async ()=>{
+    const response = await fetch(api)
+    const data = await response.json()
+    console.log(data)
+}
+
+getApiData()
+
+// const response =  fetch(api)
+
+// response.then(res=>res.json()).then(data=>{
+//     console.log(data[0].country)
+// }).catch(err=>{
+//     console.log(err)
+// })
 
 
-console.log(response)
+// console.log(response)
