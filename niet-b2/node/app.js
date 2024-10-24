3139641b445e257976f0f1a00a6073bfc073c655
sudhanshu kumar <sudhanshukuar8816@gmail.com>
2024-10-24 15:51:09 +0530

    more codes

--- niet-b2/node/app.js
+++ niet-b2/node/app.js
@@ -1,18 +1,41 @@
 const http = require("http")
+const fs = require('fs')
 
+const server = http.createServer((req, res) => {
 
-const server = http.createServer((req,res)=>{
-   
     res.writeHead(200, {
-            'Content-Type':'text/html'
+        'Content-Type': 'text/html'
     })
-
-    res.write("<h2>Hello i am coming back to you as a response</h2>")
+    const path = req.url
+    if(path === "/"){
+        try {
+            const data = fs.readFileSync("./views/homepage.html")
+            res.write(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }else if(path === "/login"){
+        try {
+            const data = fs.readFileSync("./views/login.html")
+            res.write(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }else if(path === "/service"){
+        try {
+            const data = fs.readFileSync("./views/services.html")
+            res.write(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }else{
+        res.write("<h2>Path does nt exist !!</h2>")
+    }
     res.end()
 
 })
 
 
-server.listen(8081,()=>{
-    console.log("server started at port 8081")
+server.listen(8083, () => {
+    console.log("server started at port 8083")
 })
